7cecdaeca5c637169ed916dc047a18b40cb80789
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2020-07-10 17:31:47 +0100

    Begin framework for implementation.

---
+++ MATLAB/main.m
@@ -0,0 +1,41 @@
+% Using only one spatial dimension for simplicity, like we will in TriMGRIT
+global space_steps
+global time_steps
+space_steps = 8;
+time_steps = 8;
+m = rand(space_steps * time_steps, 1); % Could pick different initial conditions
+rho = rand(time_steps, 1);
+lambda = rand(space_steps * time_steps + time_steps, 1);
+
+iters = 5;
+for i=1:iters
+    b = -[get_GwL(); get_GlambdaL()];
+    A = [get_A(), (get_D())'; zeros(space_steps * time_steps + time_steps), get_S()];
+    solution = linsolve(A, b);
+end
+
+
+function A = get_A()
+    global space_steps
+    global time_steps
+end
+
+function D = get_D()
+    global space_steps
+    global time_steps
+end
+
+function S = get_S()
+    global space_steps
+    global time_steps
+end
+
+function GwL = get_GwL()
+    global space_steps
+    global time_steps
+end
+
+function GlambdaL = get_GlambdaL()
+    global space_steps
+    global time_steps
+end
