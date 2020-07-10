e6f4cd197a018433e067a5df9557160ccf7aa025
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2020-07-10 17:36:25 +0100

    Complete solution framework -- just need values

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,6 +1,9 @@
 % Using only one spatial dimension for simplicity, like we will in TriMGRIT
 global space_steps
 global time_steps
+global m
+global rho
+global lambda
 space_steps = 8;
 time_steps = 8;
 m = rand(space_steps * time_steps, 1); % Could pick different initial conditions
@@ -10,8 +13,13 @@ lambda = rand(space_steps * time_steps + time_steps, 1);
 iters = 5;
 for i=1:iters
     b = -[get_GwL(); get_GlambdaL()];
+    disp(norm(b)); % Quick and dirty way to check convergence.
     A = [get_A(), (get_D())'; zeros(space_steps * time_steps + time_steps), get_S()];
     solution = linsolve(A, b);
+    alpha = line_search(solution);
+    m = m + alpha * solution(1 : space_steps * time_steps);
+    rho = rho + alpha * solution(space_steps * time_steps + 1 : space_steps * time_steps + time_steps);
+    lambda = lambda + alpha * solution(space_steps * time_steps + time_steps + 1 : (space_steps * time_steps + time_steps) * 2);
 end
 
 
@@ -38,4 +46,7 @@ end
 function GlambdaL = get_GlambdaL()
     global space_steps
     global time_steps
+end
+
+function alpha = line_search(solution)
 end
