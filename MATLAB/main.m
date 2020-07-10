bf4f8d7eb7ea4b4e9b1b51020900052d3087a301
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2020-07-10 18:19:02 +0100

    Derivative matrices

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -4,11 +4,24 @@ global time_steps
 global m
 global rho
 global lambda
+global q
+global h
+
 space_steps = 8;
 time_steps = 8;
 m = rand(space_steps * time_steps, 1); % Could pick different initial conditions
 rho = rand(time_steps, 1);
 lambda = rand(space_steps * time_steps + time_steps, 1);
+q = sparse(space_steps * (time_steps + 1));
+
+time = 1;
+h = time/time_steps;
+
+% Initial and final conditions
+q(1:space_steps/2) = ones(space_steps/2, 1);
+q(space_steps * time_steps + 1 + space_steps/2 : space_steps * time_steps + space_steps) = ones(space_steps/2, 1);
+
+q = q * (1/h);
 
 iters = 5;
 for i=1:iters
@@ -22,6 +35,32 @@ for i=1:iters
     lambda = lambda + alpha * solution(space_steps * time_steps + time_steps + 1 : (space_steps * time_steps + time_steps) * 2);
 end
 
+function D = get_derivative_matrix_time()
+    global time_steps
+    global h
+    D = sparse(time_steps);
+    D(1, 1) = 2;
+    for i=2:time_steps
+        D(i, i) = 1;
+        D(i, i-1) = -1;
+    end
+    D = (1/h) * D;
+end
+
+function D = get_derivative_matrix_space()
+    global space_steps
+    global time_steps
+    global h
+    D = sparse(space_steps);
+    D(1, 1) = 2;
+    for i=2:space_steps
+        D(i, i) = 1;
+        D(i, i-1) = -1;
+    end
+    D = (1/h) * D;
+    Drep = repmat({D}, 1, time_steps);
+    D = blkdiag(Drep{:});
+end
 
 function A = get_A()
     global space_steps
