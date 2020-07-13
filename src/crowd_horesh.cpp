8dd0f0bc6f7543a719abf3486d445dc9498cedea
Torrencem <matt@torrencefamily.net>
2020-07-13 11:01:07 -0400

    add block_diag to crowd_horesh

--- src/crowd_horesh.cpp
+++ src/crowd_horesh.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
-#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
+using Eigen::SparseMatrix;
 using Eigen::MatrixXd;
 
-int main() {
-    MatrixXd m(2, 2);
+SparseMatrix<double> block_diag(SparseMatrix<double> diag, int ncopies) {
+    auto m2 = SparseMatrix<double> (diag.rows() * ncopies, diag.cols() * ncopies);
 
-    m(0, 0) = 3;
-    m(1, 0) = 2.5;
-    m(0, 1) = -1;
-    m(1, 1) = m(1, 0) + m(0, 1);
+    for (int l = 0; l < ncopies; l++) {
+        // For each non-zero entry in diag
+        for (int k = 0; k < diag.outerSize(); k++)
+            for (SparseMatrix<double>::InnerIterator it(diag, k); it; ++it) {
+                m2.insert(l * diag.rows() + it.row(), l * diag.cols() + it.col())
+                    = it.value();
+            }
+    }
+
+    return m2;
+}
+
+void test_block_diag() {
+    SparseMatrix<double> m(3, 3);
+
+    m.insert(0, 0) = 1;
+    m.insert(1, 2) = 2;
+    m.insert(1, 0) = 3;
+    m.insert(2, 0) = 4;
+    std::cout << "M is:" << std::endl;
     std::cout << m << std::endl;
+    
+    auto m2 = block_diag(m, 3);
+    
+    std::cout << std::endl << "block_diag(M, 3) is:" << std::endl;
+    std::cout << m2 << std::endl;
+}
+
+int main() {
+    test_block_diag();
 }
