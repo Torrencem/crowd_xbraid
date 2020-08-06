24712ab80600021d8716664f3934324dabc4a759
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2020-08-06 11:50:49 +0100

    Implement decreasing normalization coefficients

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,5 +1,3 @@
-clc
-clear
 % Using only one spatial dimension for simplicity, like we will in TriMGRIT
 global space_steps
 global time_steps
@@ -11,12 +9,20 @@ global d_time
 global d_space
 global A_hat
 global D
+global D1
+global D2
 global At
 global As
 global filler_zeros
+global Gm
+global Grho
+global Glambda
+global S
+global K
+global X
 
-space_steps = 4;
-time_steps = 4;
+space_steps = 24;
+time_steps = 24;
 show = @(x) surf(reshape(x, [space_steps, time_steps+1]));
 showm = @(x) surf(reshape(x, [space_steps+1, time_steps]));
 showl = @(x) surf(reshape(x, [space_steps, time_steps+2]));
@@ -35,8 +41,21 @@ d_space = 1/space_steps;
 %q(space_steps * (time_steps + 1)+ 1 : space_steps * (time_steps + 3/2)) = ones(space_steps/2, 1);
 %q(space_steps * (time_steps + 3/2) + 1 : space_steps * (time_steps + 2)) = zeros(space_steps/2, 1) + 0.1;
 
-q(1:space_steps) = arrayfun(@(x) 0.5, 0:1/(space_steps-1):1);
-q(space_steps * (time_steps + 1) + 1: space_steps * (time_steps + 2)) = -1*arrayfun(@(x) 0.5, 0:1/(space_steps-1):1);
+K = zeros(space_steps, space_steps+1);
+for i=1:space_steps
+    K(i, i) = -1;
+    K(i, i+1) = 1;
+end
+K = K/d_space;
+
+X = zeros(space_steps+1, space_steps);
+for i=1:space_steps
+    X(i, i) = 1/4;
+    X(i+1, i) = 1/4;
+end
+
+q(1:space_steps) = arrayfun(@(x) 0.3*expodist(x, 0.7) + 0.2*expodist(x, 0.2) +0.1, 0:1/(space_steps-1):1);
+q(space_steps * (time_steps + 1) + 1: space_steps * (time_steps + 2)) = -1*arrayfun(@(x) 0.5*expodist(x, 0.4)+0.1, 0:1/(space_steps-1):1);
 
 q = q * (1/d_time);
 
@@ -46,22 +65,19 @@ if abs(sum(q(1:space_steps)) + sum(q(space_steps * (time_steps + 1) + 1: space_s
     disp("Conservation of mass is false.  This is not a problem, but might lead to seemingly strange behaviour.");
 end
 
-iters = 1;
+iters = 5;
 for i=1:iters
-    recalc_matrices(m, rho)
+    normcoeff = 0.0001*2^(-i);
+    recalc_matrices(m, rho, normcoeff);
+    sizeD = size(D);
+    b = D*inv(A_hat)*get_GwL(m, rho, lambda) - get_GlambdaL(m, rho);
+    
+    dlambda = S\b;
+    dw = -inv(A_hat)*(D'*dlambda+get_GwL(m, rho, lambda));
+    dm = dw(1 : (space_steps + 1) * time_steps);
+    drho = dw((space_steps + 1) * time_steps + 1 : (space_steps + 1) * time_steps + space_steps * (time_steps + 1));
 
-    A = [A_hat, D'; D, filler_zeros];
-    b = -[get_GwL(m, rho, lambda); get_GlambdaL(m, rho)];
-    
-    disp(norm(b)/(space_steps*time_steps)); % Quick and dirty way to check convergence.
-    
-    solution = A\b;
-    dm = solution(1 : (space_steps + 1) * time_steps);
-    drho = solution((space_steps + 1) * time_steps + 1 : (space_steps + 1) * time_steps + space_steps * (time_steps + 1));
-    dlambda = solution((space_steps + 1) * time_steps + space_steps * (time_steps + 1) + 1 : length(solution));
-
-    alpha = line_search(dm, drho, dlambda);
-    
+    alpha = line_search(dm, drho, dlambda, normcoeff);
     m = m + alpha * dm;
     m = m * 1;
     rho = rho + alpha * drho;
@@ -85,10 +101,55 @@ function calc_fixed_matrices()
     filler_zeros = zeros(dim_d(1));
 end
 
-function recalc_matrices(m, rho)
+function recalc_matrices(m, rho, normcoeff)
     global A_hat
+    global S
+    global D
+    dim_D = size(D);
     A_hat = get_A_hat(m, rho);
+%     S = -(D / A_hat) * D';
+    S = get_S(m, rho, normcoeff);
 end
+
+function S = get_S(m, rho, normcoeff)
+    global K
+    global space_steps
+    global time_steps
+    global d_time
+    S = zeros(space_steps*(time_steps+2));
+    S(1:space_steps, 1:space_steps) = inv(Q(0, m, rho, normcoeff))/d_time^2;
+    S(1:space_steps, space_steps+1:2*space_steps) = -inv(Q(0, m, rho, normcoeff))/d_time^2;
+    for i=1:time_steps
+        S(i*space_steps+1:(i+1)*space_steps, (i-1)*space_steps+1:i*space_steps) = -inv(Q(i-1, m, rho, normcoeff))/d_time^2;
+        S(i*space_steps+1:(i+1)*space_steps, (i+1)*space_steps+1:(i+2)*space_steps) = -inv(Q(i, m, rho, normcoeff))/d_time^2;
+        S(i*space_steps+1:(i+1)*space_steps, i*space_steps+1:(i+1)*space_steps) = inv(Q(i-1, m, rho, normcoeff))/d_time^2+inv(Q(i, m, rho, normcoeff))/d_time^2+K*inv(P(i-1, rho))*K';
+    end
+    S((time_steps+1)*space_steps+1:end, time_steps*space_steps+1:(time_steps+1)*space_steps) = -inv(Q(time_steps, m, rho, normcoeff))/d_time^2;
+    S((time_steps+1)*space_steps+1:end, (time_steps+1)*space_steps+1:end) = inv(Q(time_steps, m, rho, normcoeff))/d_time^2;
+    S = -S;
+end
+
+function Pi = P(i, rho)
+    global X
+    global space_steps
+    Pi = 2 * diag(X*(1./(rho(i*space_steps+1:(i+1)*space_steps)) + 1./(rho((i+1)*space_steps+1:(i+2)*space_steps))));
+    Pi = Pi;
+end
+
+function Qi = Q(i, m, rho, normcoeff)
+    global X
+    global space_steps
+    global time_steps
+    if i == 0
+        Qi = 2 * diag(X'*(m(i*(space_steps+1)+1:(i+1)*(space_steps+1)).^2)) * diag(rho(i*space_steps+1:(i+1)*space_steps));
+    elseif i == time_steps
+        Qi = 2 * diag(X'*(m((i-1)*(space_steps+1)+1:i*(space_steps+1)).^2)) * diag(rho(i*space_steps+1:(i+1)*space_steps));
+    else
+        Qi = 2 * diag(X'*(m(i*(space_steps+1)+1:(i+1)*(space_steps+1)).^2+m((i-1)*(space_steps+1)+1:i*(space_steps+1)).^2)) * diag(rho(i*space_steps+1:(i+1)*space_steps));
+    end
+    Qi = Qi + normcoeff*eye(size(Qi));
+end
+
 
 function D = get_derivative_matrix_space()
     global space_steps
@@ -161,6 +222,7 @@ function A = get_A_hat(m, rho)
     vec_2 = 2 * At' * As * (m.^2);
     vec_3 = 1./(rho.^3);
     A = blkdiag(diag(vec_1), diag(vec_2) * diag(vec_3));
+    A = A + 0.00001 * eye(size(A));
 end
 
 function GwL = get_GwL(m, rho, lambda)
@@ -168,31 +230,37 @@ function GwL = get_GwL(m, rho, lambda)
     global At
     global D1
     global D2
+    global Gm
+    global Grho
     GmL = 2 * diag(m) * As' * At * (1./rho) + D1' * lambda;
     GrhoL = -diag(1./(rho.^2)) * At' * As * (m.^2) + D2' * lambda;
+    Gm = GmL;
+    Grho = GrhoL;
     GwL = [GmL; GrhoL];
 end
 
 function GlambdaL = get_GlambdaL(m, rho)
     global q
     global D
+    global Glambda
     GlambdaL = D * [m; rho] - q;
+    Glambda = GlambdaL;
 end
 
-function reward = reward(x, dm, drho, dlambda)
+function reward = reward(x, dm, drho, dlambda, normcoeff)
     global m
     global rho
     global lambda
     newm = m + x * dm;
     newrho = rho + x * drho;
     newlambda = lambda + x * dlambda;
-    recalc_matrices(newm, newrho)
+    recalc_matrices(newm, newrho, normcoeff)
     b = -[get_GwL(newm, newrho, newlambda); get_GlambdaL(newm, newrho)];
     reward = norm(b);
 end
 
-function alpha = line_search(dm, drho, dlambda)
-    f = @(x) reward(x, dm, drho, dlambda);
+function alpha = line_search(dm, drho, dlambda, normcoeff)
+    f = @(x) reward(x, dm, drho, dlambda, normcoeff);
     options = optimoptions(@fminunc, 'Display', 'none');
     alpha = fminunc(f, 1, options); 
     
